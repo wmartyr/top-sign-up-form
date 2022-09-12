230a1733dd91a72c47c76b83a6a664f64eb24027
wmartyr <woodrow.martyr@gmail.com>
2022-09-12 19:42:22 +0930

    Implement password checker

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,19 @@
 const password = document.querySelector(".password");
 const confirmPassword = document.querySelector(".confirm-password");
+const form = document.querySelector("form");
 
-if password.value
+function checkPasswords() {
+    if (password.value !== "" && password.value === confirmPassword.value) {
+        form.classList.add("no-message");
+        password.classList.remove("error");
+        confirmPassword.classList.remove("error");
+    } else {
+        form.classList.remove("no-message");
+        password.classList.add("error");
+        confirmPassword.classList.add("error");
+    }
+}
+
+password.addEventListener("input", checkPasswords);
+confirmPassword.addEventListener("input", checkPasswords);
+
